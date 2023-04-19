7fbd2cf6eb8d1bddbbf29927789977e934a42a1f
Корниенко Сергей Дмитриевич <skornienko@t1-consulting.ru>
2023-04-19 10:26:33 +0300

    fix v.20.1

--- src/mainApp/hooks/useIOSocketNotify.ts
+++ src/mainApp/hooks/useIOSocketNotify.ts
@@ -8,46 +8,67 @@ import { Notify } from "../../types";
 const URL = import.meta.env.VITE_BASE_URL;
 
 export const useIOSocketNotify = () => {
+  const socketClientRef = useRef<any>(null);
   const socketNotifyRef = useRef<any>(null);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-      io(URL, {
-        path: "",
-        extraHeaders: {
-          Authorization: `Bearer ${tokenService.getJwtToken()}`,
-        },
-      });
+    const socketClient = io(URL, {
+      path: "",
+      extraHeaders: {
+        Authorization: `Bearer ${tokenService.getJwtToken()}`,
+      },
+      forceNew: true,
+    });
 
-      const socketNotify = io(`${URL}/notification`);
+    const socketNotify = io(`${URL}/notification`, {
+      extraHeaders: {
+        Authorization: `Bearer ${tokenService.getJwtToken()}`,
+      },
+      forceNew: true,
+    });
 
-      socketNotify.on("connect", () => {
-        console.log("Connect socketNotify");
-      });
+    socketNotify.on("connect", () => {
+      console.log("Connect socketNotify");
+    });
+
+    socketClient.on("connect", () => {
+      console.log("Connect socketClient");
+    });
+
+    socketClient.on("disconnect", () => {
+      console.log("Disconnected socketClient from server");
+    });
+
+    socketNotify.on("disconnect", () => {
+      console.log("Disconnected socketNotify from server");
+    });
+
+    socketNotify.emit("notification:get", (data: Notify[]) => {
+      console.log("notification:get", data);
+      dispatch(setListOfNotify(data));
+    });
+
+    socketNotify.on("notification:new", (data) => {
+      console.log("notification:new", data);
 
       socketNotify.emit("notification:get", (data: Notify[]) => {
         console.log("notification:get", data);
         dispatch(setListOfNotify(data));
       });
+    });
 
-      socketNotify.on("notification:new", (data) => {
-        console.log("notification:new", data);
+    socketNotifyRef.current = socketNotify;
+    socketClientRef.current = socketClient;
 
-        socketNotify.emit("notification:get", (data: Notify[]) => {
-          console.log("notification:get", data);
-          dispatch(setListOfNotify(data));
-        });
-      });
+    dispatch(setNotifyRef(socketNotifyRef));
 
-      socketNotifyRef.current = socketNotify;
-
-      dispatch(setNotifyRef(socketNotifyRef));
-
-      return () => {
-        socketNotifyRef.current.disconnect();
-        dispatch(setNotifyRef(null));
-      };
+    return () => {
+      socketNotifyRef.current.disconnect();
+      socketClientRef.current.disconnect();
+      dispatch(setNotifyRef(null));
+    };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
