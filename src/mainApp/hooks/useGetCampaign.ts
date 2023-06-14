4d7522f112e178cb53c6e92338ee722be54dec0a
Корниенко Сергей Дмитриевич <>
2023-06-14 13:06:42 +0300

    fix: [0606-6]

---
+++ src/mainApp/hooks/useGetCampaign.ts
@@ -0,0 +1,30 @@
+import { AxiosError } from "axios";
+import { getCampaign } from "../../request";
+import { openNotificationWithIcon } from "../../utils";
+import { setClientCampaign } from "../context/action";
+import { useDispatch } from "../context/hooks";
+
+export const useGetCampaign = () => {
+  const dispatch = useDispatch();
+
+  const handleGetCampaign = async () => {
+    try {
+      const response = await getCampaign(false);
+      const campaignArray = response.data.result.map((item) => ({
+        ...item,
+        type: "campaign",
+      }));
+      dispatch(setClientCampaign(campaignArray));
+    } catch (err) {
+      const typedError = err as AxiosError;
+      openNotificationWithIcon({
+        type: "error",
+        message: "",
+        description: "Не удалось загрузить проекты",
+        status: typedError.status,
+      });
+    }
+  };
+
+  return { handleGetCampaign };
+};
