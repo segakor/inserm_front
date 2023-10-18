790f9f2853a9766d3ef9aef820050dad6bbf3191
Корниенко Сергей Дмитриевич <>
2023-10-18 12:39:20 +0300

    fix validation

--- src/mainApp/components/CreateCampaign/CreateCampaignPlatform.tsx
+++ src/mainApp/components/CreateCampaign/CreateCampaignPlatform.tsx
@@ -117,12 +117,11 @@ export const CreateCampaignPlatform = () => {
             { required: true, message: "Обязательное поле" },
             {
               validator: (_, value) => {
-                if (/[`~!@#$%^&*()_|+=?;:'"<>]/gi.test(value)) {
+                if (/[`~!@#$%^&*()_|+=?;:'"<>”‘]/gi.test(value)) {
                   return Promise.reject(
-                    "Спецсимволы ~!@#$%^&*()_|+=?;:'<> недоступны"
+                    "Спецсимволы ~!@#$%^&*()_|+=?;:'<>”‘ недоступны"
                   );
                 }
-                return Promise.resolve();
               },
             },
           ]}
