e1a21d96c9cccc64b2f3ed379777b08c59675543
Корниенко Сергей Дмитриевич <>
2024-01-15 19:05:41 +0300

    update logo

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    },
+}
 });
